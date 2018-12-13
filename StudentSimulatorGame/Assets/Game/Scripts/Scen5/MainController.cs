a5dfb1a9c83954b8d7052d68e413f420790a6a20
Reket <dmitry_rekechinsky@i.ua>
2018-12-13 20:42:14 +0200

    add projeckt files. bag fix. add image and sounds

---
+++ StudentSimulatorGame/Assets/Game/Scripts/Scen5/MainController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using PSV_Prototype;
+using DG.Tweening;
+
+public class SceneContoller : MonoBehaviour {
+    [SerializeField]
+    private GameObject _bg;
+	// Use this for initialization
+	void Start () {
+        var time = AudioPlayer.Instance.Play("scoolEnd");
+        _bg.GetComponent<SpriteRenderer>().DOFade(1f, time);
+        DOVirtual.DelayedCall(AudioPlayer.Instance.Play("scoolEnd"), delegate {
+            SceneLoader.Instance.SwitchToScene(Scenes.Abiturient);
+        }, false);
+
+    }
+}
